62c6c2ddceced9df177f6099f41e4973cb5e36d5
Pavel <you@example.com>
2024-01-13 13:22:15 +0300

    fg

--- resources/js/Components/ui/index.js
+++ resources/js/Components/ui/index.js
@@ -1,8 +1,8 @@
-import UInput from '@/components/ui/UInput.vue'
-import ULoader from '@/components/ui/ULoader.vue'
-import UNavigate from '@/components/ui/UNavigate.vue'
-import UButton from '@/components/ui/UButton.vue'
-import Footer from '@/components/ui/Footer.vue'
+import UInput from '../../components/ui/UInput.vue'
+import ULoader from '../../components/ui/ULoader.vue'
+import UNavigate from '../../components/ui/UNavigate.vue'
+import UButton from '../../components/ui/UButton.vue'
+import Footer from '../../components/ui/Footer.vue'
 
 const components = [
     {name: 'UInput', component: UInput},
