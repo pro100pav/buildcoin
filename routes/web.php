f82477ee2899769a136dee03afad61b6938cf925
Pavel <you@example.com>
2024-01-13 13:33:57 +0300

    fg

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Welcome');
+    return view('welcome');
 })
 ->name('home');
 Route::get('/profile', function () {
-    return view('Profile');
+    return view('profile');
 })
 ->name('profile');
